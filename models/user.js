ea9a35c9fbed6a0b3354fe6c775c1f76c9d87003
CHIRANJEET DWIVEDY <154692196+CHIRANJEET12@users.noreply.github.com>
2024-06-19 19:10:18 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,19 +2,41 @@ const mongoose = require("mongoose");
 
 // Define the user schema
 const userSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+        unique: true // Assuming userId should be unique
+    },
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true // Automatically remove whitespace from name
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true, // Convert email to lowercase before saving
+        trim: true, // Automatically remove whitespace from email
+        match: [/\S+@\S+\.\S+/, 'is invalid'] // Email validation
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: 6 // Minimum length for password
+    },
+    cardnumber: {
+        type: String,
+        required: true,
+        unique: true 
+    },
+    balance: {
+        type: String,
+        required: true,
+        default: '0' // Default balance is 0
     }
+}, {
+    timestamps: true // Automatically add createdAt and updatedAt timestamps
 });
 
 // Create a User model based on the user schema
